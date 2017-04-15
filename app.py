bbac5c7bb06f0250744384342d7060d37fffa157
Simon Bilsky-Rollins <simonbr73@gmail.com>
2017-04-15 17:07:04 -0500

    Finish writing API endpoint

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 import traceroute
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return
+@app.route('/traceroute')
+def default_traceroute():
+    geoip = traceroute.main('google.com')
+    return jsonify(geoip)
+
+@app.route('/traceroute', methods=['POST'])
+def user_traceroute():
+    if not request.json:
+        abort(400); # bad request
+    remote_server = request.json.get('remote-server')
+    geoip = traceroute.main(remote_server)
+    return jsonify(geoip)
+
+app.run(debug=True)
