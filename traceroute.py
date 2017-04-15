444f039721737ffc20380f4e7365b2b504a64bcf
Barr Iserloth <barriserloth@gmail.com>
2017-04-14 23:06:02 -0500

    Using geoip2 database, generates list of cities and coordinates for ip addresses from traceroute

--- traceroute.py
+++ traceroute.py
@@ -2,7 +2,9 @@
 
 import argparse
 import subprocess
+import geoip2.database
 
+reader = geoip2.database.Reader('/path/to/GeoLite2-City.mmdb')
 address = 'google.com'
 command = 'traceroute ' + address
 output = subprocess.check_output(command, shell=True)
@@ -13,13 +15,37 @@ split_list = split_list[1:]
 ip_name_list = []
 ip_addr_list = []
 
+# generate ip name list and ip address list
 for i in range(len(split_list)):
     line = split_list[i].split()
     for j in range(len(line)):
-        if(j == 1):
+        if j == 1:
             ip_name_list.append(line[j])
-        elif(j == 2):
+        elif j == 2:
             ip_addr_list.append(line[j])
 
-print(ip_name_list)
+# clean up ip address (remove parens)
+for i in range(len(ip_addr_list)):
+    word = ip_addr_list[i]
+    word = word.replace('(', '')
+    word = word.replace(')','')
+    ip_addr_list[i] = word
+
+# generate list of ip locations using geoip2
+ip_locs = []
+ip_lat_long = []
+
+for i in range(len(ip_addr_list)):
+    ip = ip_addr_list[i]
+    
+    if ip[:3] != '10.' and ip[:3] != '127':
+        response = reader.city(ip)
+        ip_locs.append(response.city.name)
+        ip_lat_long.append((response.location.latitude, response.location.longitude))
+
+
 print(ip_addr_list)
+print
+print(ip_locs)
+print
+print(ip_lat_long)
