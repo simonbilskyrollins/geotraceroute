041af3c4e3dc9a39da4066c31cb510154d872ad0
Simon Bilsky-Rollins <simonbr73@gmail.com>
2017-04-15 17:07:33 -0500

    Add points to map asynchronously

--- map.js
+++ map.js
@@ -2,15 +2,15 @@ var geoip = [];
 var points = [];
 
 jQuery.ajax({
-  url: "http://localhost:5000/",
+  url: "http://localhost:5000/traceroute",
   type: "GET",
-
   contentType: 'application/json; charset=utf-8',
   success: function(resultData) {
-    geoip = resultData;
+    addPoints(resultData);
   },
-  error: function(jqXHR, textStatus, errorThrown) {},
-
+  error: function(jqXHR, textStatus, errorThrown) {
+    console.log(errorThrown);
+  },
   timeout: 120000,
 });
 
@@ -20,25 +20,29 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
   maxZoom: 18,
   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    'Imagery &copy; <a href="http://mapbox.com">Mapbox</a>' + ' | ' +
+    'This product includes GeoLite2 data created by MaxMind, available from ' +
+    '<a href="http://www.maxmind.com">http://www.maxmind.com</a>.',
   id: 'mapbox.streets'
 }).addTo(mymap);
 
-source = geoip[0]
-if (source != null) {
-  mymap.setView([source.latitude, source.longitude], 5);
-} else {
-  mymap.setView([15.0, 0.0], 3);
-}
+function addPoints(geoip) {
+  source = geoip[0];
+  if (source != null) {
+    mymap.setView([source.latitude, source.longitude], 4);
+  } else {
+    mymap.setView([15.0, 0.0], 3);
+  }
 
-for (i = 0; i < geoip.length; i++) {
-  current_geoip = geoip[i];
-  domain = current_geoip.domain;
-  lat = current_geoip.latitude;
-  long = current_geoip.longitude;
-  var point = L.marker([lat, long]).addTo(mymap)
-    .bindPopup(domain);
-  points.push(point);
+  for (i = 0; i < geoip.length; i++) {
+    current_geoip = geoip[i];
+    domain = current_geoip.domain;
+    lat = current_geoip.latitude;
+    long = current_geoip.longitude;
+    point = L.marker([lat, long]).addTo(mymap)
+      .bindPopup(domain);
+    points.push(point);
+  }
 }
 
 var popup = L.popup();
