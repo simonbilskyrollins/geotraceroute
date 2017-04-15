124aea24313b41b41030583644c0cb2f3573e2f2
Simon Bilsky-Rollins <simonbr73@gmail.com>
2017-04-15 00:30:31 -0500

    Beautify JavaScript with correct indentation

--- map.js
+++ map.js
@@ -2,26 +2,26 @@ var geoip = [];
 var points = [];
 
 jQuery.ajax({
-    url: "localhost:5000/",
-    type: "GET",
+  url: "http://localhost:5000/",
+  type: "GET",
 
-    contentType: 'application/json; charset=utf-8',
-    success: function(resultData) {
-        geoip = resultData;
-    },
-    error: function(jqXHR, textStatus, errorThrown) {},
+  contentType: 'application/json; charset=utf-8',
+  success: function(resultData) {
+    geoip = resultData;
+  },
+  error: function(jqXHR, textStatus, errorThrown) {},
 
-    timeout: 120000,
+  timeout: 120000,
 });
 
 var mymap = L.map('map');
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    id: 'mapbox.streets'
+  maxZoom: 18,
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  id: 'mapbox.streets'
 }).addTo(mymap);
 
 source = geoip[0]
@@ -32,22 +32,22 @@ if (source != null) {
 }
 
 for (i = 0; i < geoip.length; i++) {
-    current_geoip = geoip[i];
-    domain = current_geoip.domain;
-    lat = current_geoip.latitude;
-    long = current_geoip.longitude;
-    var point = L.marker([lat, long]).addTo(mymap)
-        .bindPopup(domain);
-    points.push(point);
+  current_geoip = geoip[i];
+  domain = current_geoip.domain;
+  lat = current_geoip.latitude;
+  long = current_geoip.longitude;
+  var point = L.marker([lat, long]).addTo(mymap)
+    .bindPopup(domain);
+  points.push(point);
 }
 
 var popup = L.popup();
 
 function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(mymap);
+  popup
+    .setLatLng(e.latlng)
+    .setContent("You clicked the map at " + e.latlng.toString())
+    .openOn(mymap);
 }
 
 mymap.on('click', onMapClick);
