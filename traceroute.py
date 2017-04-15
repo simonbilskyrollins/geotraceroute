8832643223dc89255efcf50bfc58e55142c6de12
Barr Iserloth <barriserloth@gmail.com>
2017-04-14 22:15:12 -0500

    added parsing to isolate host names/ip addresses

--- traceroute.py
+++ traceroute.py
@@ -6,4 +6,20 @@ import subprocess
 address = 'google.com'
 command = 'traceroute ' + address
 output = subprocess.check_output(command, shell=True)
-print(output)
+
+split_list = output.split('\n')
+split_list = split_list[1:]
+
+ip_name_list = []
+ip_addr_list = []
+
+for i in range(len(split_list)):
+    line = split_list[i].split()
+    for j in range(len(line)):
+        if(j == 1):
+            ip_name_list.append(line[j])
+        elif(j == 2):
+            ip_addr_list.append(line[j])
+
+print(ip_name_list)
+print(ip_addr_list)
