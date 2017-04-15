9988e0a512151f9c316dc57be072acebac9fc080
Simon Bilsky-Rollins <simonbr73@gmail.com>
2017-04-15 16:50:33 -0500

    Minor refactoring to play nice with Flask

--- traceroute.py
+++ traceroute.py
@@ -5,16 +5,16 @@ import subprocess
 import re
 import geoip2.database
 
-reader = geoip2.database.Reader('GeoLite2-City.mmdb')
-
-def main():
-    address = 'google.com'
+def main(address):
+    reader = geoip2.database.Reader('GeoLite2-City.mmdb')
     command = 'traceroute ' + address
     output = subprocess.check_output(command, shell=True)
-    return parse_route(output)
+    geoip = parse_route(reader, output)
+    reader.close()
+    return geoip
 
 # generate ip name list and ip address list
-def parse_route(output):
+def parse_route(reader, output):
     split_list = output.split('\n')
     split_list = split_list[1:]
 
@@ -29,15 +29,15 @@ def parse_route(output):
                 pass
             domain = hop_match.group(2)
             ip_addr = hop_match.group(3)
-            latitude, longitude = get_ip_location(ip_addr)
+            latitude, longitude = get_ip_location(reader, ip_addr)
             hop_dict = {'hop': hop, 'domain': domain, 'ip': ip_addr,
                         'latitude': latitude, 'longitude': longitude}
             hops.append(hop_dict)
     return hops
 
 # generate list of ip locations using geoip2
-def get_ip_location(ip):
+def get_ip_location(reader, ip):
     if ip[:3] != '10.' and ip[:3] != '127':
-        response = reader.city(ip)
-        location = response.location
+        city = reader.city(ip)
+        location = city.location
         return location.latitude, location.longitude
