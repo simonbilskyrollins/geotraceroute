6556f6702e679e0bf8b717a3b2329979959bbfdb
Simon Bilsky-Rollins <simonbr73@gmail.com>
2017-04-15 17:13:10 -0500

    Attribution style tweak

--- map.js
+++ map.js
@@ -4,7 +4,7 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
   maxZoom: 18,
   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-    'Imagery &copy; <a href="http://mapbox.com">Mapbox</a>' + ' | ' +
+    'Imagery &copy; <a href="http://mapbox.com">Mapbox</a>' + '<br/>' +
     'This product includes GeoLite2 data created by MaxMind, available from ' +
     '<a href="http://www.maxmind.com">http://www.maxmind.com</a>.',
   id: 'mapbox.streets'
